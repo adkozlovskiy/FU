b5a42b02b4da1eb6735325c22a74fa60197a1402
whiititi <69580767+whiititi@users.noreply.github.com>
2020-09-08 18:36:50 +0300

    //

---
+++ Современные технологии программирования/Practic One/Main.java
@@ -0,0 +1,29 @@
+//package com.company
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+  static public void main(String[] args){
+    Scanner in = new Scanner(System.in);
+
+    System.out.print("Введите высоту матрицы A: ");
+    int Aheight = in.nextInt();
+    System.out.print("Введите ширину матрицы A: ");
+    int Awidth = in.nextInt();
+
+    System.out.print("Введите высоту матрицы B: ");
+    int Bheight = in.nextInt();
+    System.out.print("Введите ширину матрицы B: ");
+    int Bwidth = in.nextInt();
+
+    Matrix matrix1 = new Matrix(Aheight, Awidth);
+    Matrix matrix2 = new Matrix(Bheight, Bwidth);
+
+    try{
+      int[][] result = matrix1.addToMatrix(matrix2);
+    } catch (Exception exception){
+      System.out.println("Ошибка. Неодинаковый matrix volume!");
+    }
+  }
+}
