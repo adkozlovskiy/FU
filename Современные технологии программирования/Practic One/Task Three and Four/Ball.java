498c36f9df7d6c3a457fc50178f677f35b7b62c1
whiititi <69580767+whiititi@users.noreply.github.com>
2020-09-12 17:19:42 +0300

    //

---
+++ Современные технологии программирования/Practic One/Task Three and Four/Ball.java
@@ -0,0 +1,12 @@
+package com.company;
+
+class Cylinder extends SolidOfRevolution {
+
+    // Constructor double h, r -->
+    // SolidOfRevolution.h, SolidOfRevolution.r --> Shape.v
+
+    Cylinder (double height, double radius){
+        super(radius, height);
+    }
+
+}
