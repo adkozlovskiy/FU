aa5eba52479ce8ab8247af17b711c585a2de2647
whiititi <69580767+whiititi@users.noreply.github.com>
2020-09-12 17:17:39 +0300

    //

---
+++ Современные технологии программирования/Practic One/Task Three and Four/Pyramid.java
@@ -0,0 +1,9 @@
+//package com.company;
+
+class Pyramid extends Shape {
+
+    // Constructor double s, h --> Shape.v
+    Pyramid(double s, double height) {
+        super(1.0 / 3.0 * s * height);
+    }
+}
