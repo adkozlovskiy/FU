b10baa8a7116f076cb71f33e9b3e72a55654a46d
whiititi <69580767+whiititi@users.noreply.github.com>
2020-09-14 21:46:01 +0300

    //

---
+++ Современные технологии программирования/Practic One/Task Three and Four/Ball.java
@@ -0,0 +1,9 @@
+package com.company;
+
+class Ball extends SolidOfRevolution {
+
+    // Constructor double r --> SolidOfRevolution.r --> Shape.v
+    Ball(double radius) {
+        super(radius);
+    }
+}
