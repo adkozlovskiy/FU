c21f71182b3beb2a1028f7441c856f5c713aafa0
Alexey Kozlovskiy <whiititi@gmail.com>
2021-02-15 21:18:52 +0300

    Delete Современные технологии программирования/Practic One directory

--- Современные технологии программирования/Practic One/Task Three and Four/Pyramid.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-class Pyramid extends Shape {
-
-    // Constructor double s, h --> Shape.v
-    Pyramid(double s, double height) {
-        super(1.0 / 3.0 * s * height);
-    }
-}
