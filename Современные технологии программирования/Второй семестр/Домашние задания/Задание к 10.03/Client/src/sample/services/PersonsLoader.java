6829b5c515e4750b651805cdeda1ca1814903bfe
Alexey Kozlovskiy <whiititi@gmail.com>
2021-03-11 08:06:04 +0300

    Code clean-up.

--- Современные технологии программирования/Второй семестр/Домашние задания/Задание к 10.03/Client/src/sample/services/PersonsLoader.java
+++ Современные технологии программирования/Второй семестр/Домашние задания/Задание к 10.03/Client/src/sample/services/PersonsLoader.java
@@ -13,18 +13,21 @@ public class PersonsLoader {
 
     private static final MediaType JSON
             = MediaType.get("application/json; charset=utf-8");
+    
+    private OkHttpClient client;
+    private Gson gson = new Gson();
 
     public PersonsLoader() {
+        client = new OkHttpClient();
     }
 
     public List<Person> getPersons() throws Exception {
         URL url = new URL("http://localhost:8080/persons");
 
-        Gson gson = new Gson();
+
         Type listType = new TypeToken<List<Person>>() {
         }.getType();
-
-        OkHttpClient client = new OkHttpClient();
+        
         Request request = new Request.Builder()
                 .url(url)
                 .build();
@@ -39,46 +42,31 @@ public class PersonsLoader {
 
     public void postPerson(Person person) throws Exception {
         URL url = new URL("http://localhost:8080/persons");
-
-        Gson gson = new Gson();
+        
         RequestBody body = RequestBody.create(JSON, gson.toJson(person));
-        OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(url)
                 .post(body)
                 .build();
-
-        Response response = client.newCall(request).execute();
-        if (response.body() != null) {
-            response.body().string();
-
-        }
-
+        
+        client.newCall(request).execute();
     }
 
     public void putPerson(Person person, String id) throws Exception {
         URL url = new URL("http://localhost:8080/persons/" + id);
-
-        Gson gson = new Gson();
+        
         RequestBody body = RequestBody.create(JSON, gson.toJson(person));
-        OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(url)
                 .put(body)
                 .build();
 
-        Response response = client.newCall(request).execute();
-        if (response.body() != null) {
-            response.body().string();
-
-        }
-
+        client.newCall(request).execute();
     }
 
     public void deletePerson(String id) throws Exception {
         URL url = new URL("http://localhost:8080/persons/" + id);
-
-        OkHttpClient client = new OkHttpClient();
+        
         Request request = new Request.Builder()
                 .url(url)
                 .delete()
